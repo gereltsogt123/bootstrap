b5f86d25feb3d10d4400d6de73512b069c8cae88
gereltsogt123 <gereltsogt573@gmail.com>
2023-06-21 12:02:08 +0800

    1

--- js.js
+++ js.js
@@ -1,13 +1,13 @@
 // 1. Хоёр багийн дундаж оноог олно уу. ( A team - 96, 108, 89; B team - 88, 91, 110 )
 // 2. Багийн дундаж нөгөө багийхаас их бол тэр баг түрүүлсэн гэж хэвлэнэ үү. Багуудын оноо тэнцсэн тохиолдолд багууд тэнцсэн гэж хэвлэнэ үү.
 // Энэ дүрэм нь зөвхөн багийн дундаж 100 аас багагүй тохиолдолд энэхүү дүрэм хэрэгжэхийг анхаарна уу. Хэрвээ 2 баг 2 уулаа 100 аас бага тохиолдолд ялагч байхгүй. Тус бүрийн нөхцөлийг оноог өөрчилж туршиж үзээрэй.
-var Ateam = (100 + 80 + 1000) / 3;
+var Ateam = (100 + 80 + 10) / 3;
 console.log(Ateam + "  dundaj onoo");
-var Bteam = (100 + 80 + 1001) / 3;
+var Bteam = (100 + 80 + 11) / 3;
 console.log(Bteam + "  dundaj onoo");
 if (Ateam < 100) {
   if (Bteam < 100) {
-    console.log("HOYR BAGIIN ONOO 100 HVREEEGV UCHRAAS TENTSLEE");
+    console.log("HOYR BAGIIN ONOO 100 HVREEEGV UCHRAAS YLAGCH BHGVI");
   }
 }
 // a b iin onooo 100gaas baga bish ih bas tentsvv vyd door bgaa code ajillana
