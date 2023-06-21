27ce198c2369b71191b4697a0cb6d186a3aa21e4
gereltsogt123 <gereltsogt573@gmail.com>
2023-06-21 17:54:13 +0800

    1

--- java.js
+++ java.js
@@ -1,10 +1,10 @@
-let message = "asdkasdaskdasd askdkasldalksd as dklasjdlkasd askdlasdlkasd";
-console.log(message);
-let txt = "asdasd asd asd asd asd asd asd ";
-console.log(txt);
-let txt1 = "asdasd";
-console.log(txt);
-let txt2 = "asdasd";
-console.log(txt2);
-let txt3 = "asdasd";
-console.log(txt3);
+//  Гончигсумлай хоолны газар оров. Орсон хоолны газар нь 5000₮ -
+// 30000₮ хооронд 15% нэмж төлдөг, бусад үед 20% - ийг нэмж төлдөг бол.
+// Эдгээр тохиолдолуудад нийт хэдийг төлөхийг олно уу. Эдгээр тохиолдолуудад тус бүр хэдийг төлөх вэ?
+// 3000₮, 27500₮, 100000₮. (if else ашиглахгүй ternary operator ашиглаарай).
+let une = (20 / 100) * 3000;
+une2 = (15 / 100) * 27500;
+une3 = (20 / 100) * 100000;
+console.log(une + " tanii nemj tuluh 20%iin hvv dawhar bnpodogdson");
+console.log(une2 + " tanii nemj tuluh 15%iin hvv dawhar bnpodogdson");
+console.log(une3 + " tanii nemj tuluh 20%iin hvv dawhar bnpodogdson");
