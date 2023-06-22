6c13e9df613d439cc250de41fdfa61472744f90b
Gereltsogt123 <Gereltsogt573@gmail.com>
2023-06-23 00:21:13 +0800

    1

--- zurag/eyregsurug.js
+++ zurag/eyregsurug.js
@@ -1,4 +1,4 @@
 let n = prompt("utga oruulna uu");
-if (n > 0) alert((n = "positive"));
-else if (n < 0) alert((n = "negative"));
+if (n > 0) alert((n = "eyreg"));
+else if (n < 0) alert((n = "surug"));
 else if (n == 0) alert((n = "tentsvv"));
