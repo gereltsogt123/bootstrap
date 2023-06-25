7afaa2896530a95688fdad4b3494628c22f048ad
Gereltsogt123 <Gereltsogt573@gmail.com>
2023-06-25 20:35:23 +0800

    1

---
+++ zurag/3,7vrjver.js
@@ -0,0 +1,9 @@
+var number = prompt("too oruul");
+
+if (number % 7 === 0) {
+  console.log(number + " 7-iin vrjver");
+} else if (number % 3 === 0) {
+  console.log(number + "  3-iin vrjver");
+} else {
+  console.log(number + " ali ni ch bish bn");
+}
