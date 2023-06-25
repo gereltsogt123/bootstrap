287603cb73612d951342e4dda4d92c2c2e7bcc2b
Gereltsogt123 <Gereltsogt573@gmail.com>
2023-06-25 15:38:34 +0800

    1

---
+++ zurag/hamgiinbaga.js
@@ -0,0 +1,9 @@
+// тухайн өгөгдсөн хувьсагчдаас хамгийн бага утгатай хувьсагчийг console дээр хэвлэнэ үү
+// жишээ нь: Input: 120, 33, 10, 12 output: 10
+var number1 = 120;
+var number2 = 33;
+var number3 = 10;
+var number4 = 12;
+
+var minimum = Math.min(number1, number2, number3, number4);
+console.log("Minimum value:", minimum);
