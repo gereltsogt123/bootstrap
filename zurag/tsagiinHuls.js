119c2cdcc0698e3a9dfc5b2feb13caddf493ca3c
Gereltsogt123 <Gereltsogt573@gmail.com>
2023-06-25 20:24:48 +0800

    1

---
+++ zurag/tsagiinHuls.js
@@ -0,0 +1,7 @@
+// 5. Хэрэглэгчээс цаг оруулах, цаг тутамд үнэлгээ өгөх скрипт бичээрэй. Тухайн хүний цалинг тооцно уу
+// let hours = 40
+// let ratePerHour = 28
+var tsag = prompt("  tsagaa oruulna uuu");
+var tsagiinHuls = 28;
+tsag = tsag * tsagiinHuls;
+console.log("tanii tsalin: " + tsag + "$");
